1cf5170fae126aee584f4d79647b4fb65191add6
Stephen McBride <stevemcbride3@gmail.com>
2020-04-16 22:37:26 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,9 +3,14 @@ require 'pry'
 def my_collect(array)
   i = 0
   collection = []
+<<<<<<< HEAD
   while i < array.length
     collection.push(yield array[i])
     i += 1
+=======
+  while 0 < array.length
+    collection.push(yield array[i])
+>>>>>>> de94af1693bc66462a970be6d686661eb18c7822
   end
   collection
 end
